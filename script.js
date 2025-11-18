3f987dafb007d1deb2d582d8f80eba2e78da98ce
PeyreGuillaume34 <peyreguillaume34@gmail.com>
2025-11-18 10:25:47 +0100

    UPDATE : Correction Cors

--- script.js
+++ script.js
@@ -8,7 +8,13 @@ const { Pool } = pkg;
 dotenv.config();
 const app = express();
 app.use(cors({
-    origin: ['https://adaction-front.vercel.app', 'http://localhost:5500', 'http://127.0.0.1:5500'],
+    origin: [
+        'https://adaction-front.vercel.app',
+        'https://adaction-front-alpha.vercel.app',
+        'https://adaction-front-peyre-guillaumes-projects.vercel.app',
+        'http://localhost:5500',
+        'http://127.0.0.1:5500'
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
